bb3bcfe16148fc1a6699aa2b3fb44a4496d218a5
cadetsumit <laxmikantsingh07@gmail.com>
2024-06-10 18:44:05 +0530

    Update afterinstall.sh

--- afterinstall.sh
+++ afterinstall.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install 18.0.0
+node -e "console.log('Running Node.js ' + process.version)"
 cd /home/ubuntu
 npm -f install
